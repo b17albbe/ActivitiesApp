6d7db44bc2d2177c2b137ea720e3e0ac4e811345
b17albbe <b17albbe@student.his.se>
2018-05-14 05:59:31 +0200

    cleaned up the activity_mountaindetail xml file

--- app/src/main/java/com/example/brom/activitiesapp/MountainDetailsActivity.java
+++ app/src/main/java/com/example/brom/activitiesapp/MountainDetailsActivity.java
@@ -15,18 +15,10 @@ public class MountainDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mountaindetails);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
 
         Intent intent = getIntent();
         String mountain_name = intent.getStringExtra("MOUNTAIN_NAME");
